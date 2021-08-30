9d056f6a66867f0e53907199eeb3f5cd9fb4f575
NoNameDude <62181945+NoNameDude@users.noreply.github.com>
2021-08-30 23:16:33 +0200

    removed a old testing command

--- init.lua
+++ init.lua
@@ -1,9 +1,2 @@
 dofile(minetest.get_modpath("currency").."/api.lua")
 dofile(minetest.get_modpath("currency").."/commands.lua")
-
- 
-minetest.register_chatcommand("get_coin", {
-    func = function(name, param)
-        get_money(name, 100)
-    end
-})  
